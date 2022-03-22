4f80da7a44ee356ba6c164ae78009d03e82782cc
Leotr86 <101873908+Leotr86@users.noreply.github.com>
2022-03-22 16:32:21 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-BOT_TOKEN = os.environ.get('bot_token','')
+BOT_TOKEN = os.environ.get('5115075742:AAHYNndUl9zVhUshKOFv4rASmAylbJLexWs','')
 TG_API_ID = os.environ.get('tg_api_id',0)
 TG_API_HASH = os.environ.get('tg_api_hash','')
-TG_ADMIN_USER = os.environ.get('tg_admin_user','')
+TG_ADMIN_USER = os.environ.get('topwin5','')
 ROOT_DIR = 'root'
